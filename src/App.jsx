9c0346e458416fd112755c9bc789ca765aa15b71
Alfonso Lopez <alfonsolopezmosquera@gmail.com>
2023-09-22 23:47:54 -0500

    Web icon create and porfile page render in app

--- src/App.jsx
+++ src/App.jsx
@@ -2,12 +2,17 @@ import { useState } from 'react'
 import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import './App.css'
+import NavBar from './components/NavBar/NavBar'
+import Profile from './pages/Profile.jsx'
 
 
 function App() {
   return (
     <>
-    
+    <div className='Paginita'>
+      <NavBar/>
+      <Profile/>
+    </div>
     </>
   )
 }
